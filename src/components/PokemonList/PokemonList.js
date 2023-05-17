f736ba814bd085925921e99fedd786f7b75d068d
Reelecks <robinlandais75@yahoo.com>
2023-05-17 10:57:55 +0200

    End offical

--- src/components/PokemonList/PokemonList.js
+++ src/components/PokemonList/PokemonList.js
@@ -1,12 +1,31 @@
-import React  from "react";
+import React, {useState, useEffect}   from "react";
 
 function PokemonList({ searchTerm = "" }) {
 
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+  
+  async function getData() {
+    const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100");
+    const data = await res.json(); 
+    console.log("data", data.results);
+    setPokemons(data.results); 
+  }
+
+  const filteredPokemons = pokemons.filter((pokemon) =>
+    pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
 
   return (
+
     <ul>
-    <li>test</li>
-    <li>test2</li>
+    {filteredPokemons.map((pokemon) => (
+      <li key={pokemon.name}>{pokemon.name}</li>
+    ))}
     </ul>
   );
 }
